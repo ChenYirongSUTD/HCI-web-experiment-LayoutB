e79db6503b66ac6ff82ef9f898aa5727c5fa9f44
CYRAHAHAHA <iamacyr@gmail.com>
2024-08-03 17:37:05 +0800

    got the download q4 to work

--- question4.js
+++ question4.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const downloadButton = document.querySelector(".btn.download-button");
-  // Add event listener to the Download button
-  downloadButton.addEventListener("click", function () {
+  const downloadSpan = document.querySelector(".d-flex.align-items-center");
+  // Add event listener to the Download span
+  downloadSpan.addEventListener("click", function () {
     console.log("download clicked!");
     const questionElement = document.getElementById("question4");
     questionElement.style.display = "block";
